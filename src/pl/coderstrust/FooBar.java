7cd204ab76839d8aad4fc45f854bda6296ef278b
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-03-02 14:03:03 +0100

    Zadanie 1 poprawki 2

--- src/pl/coderstrust/FooBar.java
+++ src/pl/coderstrust/FooBar.java
@@ -1,29 +1,20 @@
 package pl.coderstrust;
 
 public class FooBar {
+    public static void main(String[] args) {
+        int number = 100;
+        printFooBar(number);
+    }
     public static void printFooBar(int number) {
-
-        for (int i = 0; i < (number + 1); i++) {
+        for (int i = 0; i <=number + 1; i++) {
             System.out.print(i + " ");
-
             if (i % 3 == 0) {
                 System.out.print("Foo");
             }
-
             if (i % 5 == 0) {
                 System.out.print("Bar");
             }
-
             System.out.println();
-
         }
-
-    }
-
-    public static void main(String[] args) {
-        int number = 100;
-
-        printFooBar(number);
     }
 }
-
