9029676b87b1243430a9cf16250966a41f962001
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-03-09 21:03:25 +0100

    in progress

--- src/main/java/pl/coderstrust/pascalTriangle/PascalTriangle.java
+++ src/main/java/pl/coderstrust/pascalTriangle/PascalTriangle.java
@@ -2,21 +2,21 @@ package pl.coderstrust.pascalTriangle;
 
 public class PascalTriangle {
     public static void main(String[] args) {
-        System.out.println(factorial(4));
-//        int levels = 4;
-//        for (int i = 0; i < levels; i++) {
-//            for (int k = 0; k < (levels - i - 1); k++) {
-//                System.out.print("*");
-//            }
-//            for (int j = 0; j < (i + 1); j++) {
-//                System.out.printf("%2d", j + 1);
-//            }
-//            System.out.println();
-//        }
+        int levels = 4;
+        for (int i = 0; i < levels; i++) {
+            for (int k = 0; k < (levels - i - 1); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (i + 1); j++) {
+                System.out.printf("%2d", calcNumber(i,555));
+            }
+            System.out.println();
+        }
     }
 
-    public static int calcNumber(int wzór) {
-        int result = 0;
+    public static int calcNumber(int row, int column) {
+        int result = factorial(1);
+        //TODO:dokonczyc wzor
 
 
         return result;
