cfeeaf1fd7a90c4a9e7931fbba85b914a32bd414
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-03-16 12:51:30 +0100

    Adding tests

---
+++ src/test/java/pl/coderstrust/foobar/FooBarTest.java
@@ -0,0 +1,13 @@
+package pl.coderstrust.foobar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FooBarTest {
+    @Test
+    public void test(){
+
+    }
+
+}
