1210a4491579e0101711412c70003597400b27a3
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-03-17 10:27:58 +0100

    Fixed BubbleSort and SelectionSort

--- src/main/java/pl/coderstrust/letsSortTogether/BubbleSort.java
+++ src/main/java/pl/coderstrust/letsSortTogether/BubbleSort.java
@@ -17,16 +17,16 @@ public class BubbleSort {
         for (int i = 0; i < (array.length - 1); i++) {
             for (int j = 0; j < (array.length - 1 - i); j++) {
                 if (array[j] <= array[j + 1]) {
-                    swap(array, i, j);
+                    swap(array, j, j + 1);
                 }
             }
         }
         return array;
     }
 
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[j + 1];
-        array[j + 1] = array[j];
-        array[j] = temp;
+    private static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 }
--- src/main/java/pl/coderstrust/letsSortTogether/SelectionSort.java
+++ src/main/java/pl/coderstrust/letsSortTogether/SelectionSort.java
@@ -4,29 +4,39 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] array = {11, 232, 45, 78, 4, 31, 99};
+        int[] array = {11, 232, 45, 78, 4, 31, 99, 62, 435, 22};
         System.out.println(Arrays.toString(array));
         int[] result = sort(array);
         System.out.println(Arrays.toString(result));
     }
 
     public static int[] sort(int[] array) {
+        if (array == null) {
+            throw new IllegalArgumentException("Array cannot be empty");
+        }
         int[] sortedArray = new int[array.length];
         for (int i = 0; i < array.length; i++) {
-            int maximumIndex = findMaximumIndex(array);
-            sortedArray[i] = array[maximumIndex];
-            array[maximumIndex] = 0;
+            sortedArray[i] = array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            swap(sortedArray, i, findMaximumIndex(sortedArray, i));
         }
         return sortedArray;
     }
 
-    public static int findMaximumIndex(int[] array) {
-        int indexOfCurrentMaximumValue = 0;
-        for (int i = 0; i < array.length; i++) {
+    public static int findMaximumIndex(int[] array, int start) {
+        int indexOfCurrentMaximumValue = start;
+        for (int i = start; i < array.length; i++) {
             if (array[i] > array[indexOfCurrentMaximumValue]) {
                 indexOfCurrentMaximumValue = i;
             }
         }
         return indexOfCurrentMaximumValue;
+    }
+
+    private static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 }
