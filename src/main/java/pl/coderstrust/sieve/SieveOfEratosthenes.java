1916c59030ba55fbe8dcc31eeb7be92641c5c294
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-03-18 12:56:32 +0100

    Added counter

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -8,14 +8,16 @@ public class SieveOfEratosthenes {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please put maximum number that will limit our seach of primary numbers:");
         int maximumNumber = scan.nextInt();
-        System.out.println(Arrays.toString(sieve(maximumNumber)));
+        int[] zmienna = sieve(maximumNumber);
+        System.out.println(Arrays.toString(zmienna));
+        System.out.println(counter(zmienna));
     }
 
     public static int[] sieve(int maximumNumber) {
         int[] array = new int[maximumNumber];
         filledArray(array);
         for (int i = 2; i < array.length; i++) {
-            for (int j = i * 2; j < array.length; j += i) {
+            for (int j = i + i; j < array.length; j += i) {
                 array[j] = 0;
             }
         }
@@ -23,7 +25,26 @@ public class SieveOfEratosthenes {
     }
 
     private static void filledArray(int[] array) {
-        for (int j = 1; j < array.length; j++)
+        for (int j = 2; j < array.length; j++)
             array[j] = j;
     }
+
+    private static int counter(int[] array) {
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            //System.out.println("Moje i " + i );
+            if (array[i] != 0) {
+                a++;
+            }
+        }
+        return a;
+    }
+
+//    public static void mainn(String[] args) {
+//        int[] newArray = new int[a];
+//        if(array[i] != 0) {
+//          array[i] = newArray [i]
+    //   return array
+//    }
+
 }
