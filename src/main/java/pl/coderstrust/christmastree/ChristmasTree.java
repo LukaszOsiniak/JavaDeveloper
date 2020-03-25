c83d57772f33b22c6eb3d5d21fee72fbf16b721b
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-03-25 19:19:19 +0100

    Store values in array

--- src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmastree/ChristmasTree.java
@@ -5,24 +5,36 @@ import java.util.Scanner;
 public class ChristmasTree {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj rozmiar choinki: ");
+        System.out.println("Please put size of Christmas Tree:");
         int size = scan.nextInt();
         printChristmasTree(size);
     }
 
-    public static void printChristmasTree(int size) {
+    public static String[] printChristmasTree(int size) {
+        String[] array = new String[size];
+        String space = " ";
+        String asterisk = "*";
         for (int i = 1; i <= size; i++) {
+            StringBuilder builder = new StringBuilder();
             for (int j = 0; j < size - i; j++) {
                 System.out.print(" ");
+                builder.append(space);
             }
             for (int j = 0; j < (i * 2 - 1); j++) {
                 System.out.print("*");
+                builder.append(asterisk);
             }
             System.out.println();
+            array[i - 1] = builder.toString();
         }
+        StringBuilder rootRowBuilder = new StringBuilder();
         for (int i = 1; i < (size - 1); i++) {
             System.out.print(" ");
+            rootRowBuilder.append(space);
         }
         System.out.print("**");
+        rootRowBuilder.append(asterisk).append(asterisk);
+        array[size - 1] = rootRowBuilder.toString();
+        return array;
     }
 }
---
+++ src/test/java/pl/coderstrust/christmastree/ChristmasTreeTest.java
@@ -0,0 +1,26 @@
+package pl.coderstrust.christmastree;
+
+import org.junit.jupiter.api.Test;
+
+class ChristmasTreeTest {
+
+    @Test
+    void printChristmasTree() {
+        //Given
+        int size = 4;
+        //When
+
+        //Then
+
+    }
+
+    @Test
+    void printChristmasTree1() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+}
