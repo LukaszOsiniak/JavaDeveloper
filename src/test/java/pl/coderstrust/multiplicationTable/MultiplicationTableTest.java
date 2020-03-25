1c0e9eab0f56ad91f1072accb9ead30208904b88
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-03-25 14:47:44 +0100

    Reformated to Parametrized tests

--- src/test/java/pl/coderstrust/multiplicationTable/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplicationTable/MultiplicationTableTest.java
@@ -1,17 +1,19 @@
 package pl.coderstrust.multiplicationTable;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class MultiplicationTableTest {
-
-    @Test
-    public void shouldReturnValidTable() {
-        //Given
-        int[][] expectedArray = {{1, 2}, {2, 4}};
-        int size = 3;
-
+    @ParameterizedTest
+    @MethodSource("inputSizeAndExpectedTable")
+    public void shouldReturnValidTable(int size, int[][] expectedArray) {
         //When
         int[][] actualArray = MultiplicationTable.printMultiplicationTable(size);
         int[][] subArray = createSubArray(actualArray, 1, 1);
@@ -32,16 +34,29 @@ class MultiplicationTableTest {
         return subArray;
     }
 
-    @Test
-    public void shouldReturnCorrectValue() {
-        //Given
-        int expectedValue = 16;
-        int size = 10;
+    static Stream<Arguments> inputSizeAndExpectedTable() {
+        return Stream.of(
+                arguments(3, new int[][]{{1, 2}, {2, 4}}),
+                arguments(5, new int[][]{{1, 2, 3, 4}, {2, 4, 6, 8}, {3, 6, 9, 12}, {4, 8, 12, 16}})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("sizeExpectedValue")
+    public void shouldReturnCorrectValue(int size, int expectedValue, int rowIndex, int columnIndex) {
         //When
         int[][] actualArray = MultiplicationTable.printMultiplicationTable(size);
 
         //Then
-        assertEquals(expectedValue, actualArray[4][4]);
+        assertEquals(expectedValue, actualArray[rowIndex][columnIndex]);
 
+    }
+
+    static Stream<Arguments> sizeExpectedValue() {
+        return Stream.of(
+                arguments(5, 16, 4, 4),
+                arguments(6, 20, 4, 5),
+                arguments(10, 72, 8, 9)
+        );
     }
 }
