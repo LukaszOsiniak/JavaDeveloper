2a50566f4278d2fa4d465784cd3a2b6b16cb0ee1
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-03-26 14:34:49 +0100

    Added new arguments in unit test

--- src/test/java/pl/coderstrust/christmastree/ChristmasTreeTest.java
+++ src/test/java/pl/coderstrust/christmastree/ChristmasTreeTest.java
@@ -1,50 +1,29 @@
 package pl.coderstrust.christmastree;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ChristmasTreeTest {
     @ParameterizedTest
-    @MethodSource("inputAndExpectedOutput")
+    @MethodSource("inputAndExpectedOutputAtRow")
     void printChristmasTree(int size, String lineExpected, int row) {
-        //Given
-        //int size = 4;
         //When
         String[] actual = ChristmasTree.printChristmasTree(size);
         //Then
         assertEquals(lineExpected, actual[row]);
     }
 
-    static Stream<Arguments> inputAndExpectedOutput() {
+    static Stream<Arguments> inputAndExpectedOutputAtRow() {
         return Stream.of(
                 arguments(5, "  *****", 2),
-                arguments(7, "*************", 6)
+                arguments(7, "*************", 6),
+                arguments(12, "          **", 12)
         );
     }
-
-//    @ParameterizedTest
-//    @MethodSource("trunkAtTheRightPlace")
-//    @Test
-//    void printChristmasTree1(int size, String[] arrayExpected) {
-//        //Given
-//
-//        //When
-//        String[] actual = ChristmasTree.printChristmasTree(size);
-//        //Then
-//        assertEquals(arrayExpected, actual[size]);
-//    }
-//    static Stream<Arguments> trunkAtTheRightPlace() {
-//        return Stream.of(
-//                arguments(5, new String[], 3),
-//                arguments(7, )
-//        );
-//    }
 }
