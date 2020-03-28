58ccc88b20dfcf5f181d506bc10e8bf2e29e6d7d
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-03-28 20:19:57 +0100

    Reformated printPascalTriangle Added Parametrized tests

--- src/main/java/pl/coderstrust/pascal/PascalTriangle.java
+++ src/main/java/pl/coderstrust/pascal/PascalTriangle.java
@@ -5,21 +5,32 @@ import java.util.Scanner;
 public class PascalTriangle {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj liczbę poziomów:");
+        System.out.println("Please put number of levels in Pascal triangle:");
         int levels = scan.nextInt();
         printPascalTriangle(levels);
     }
 
     public static void printPascalTriangle(int levels) {
+        int[][] matrix = createPascalTriangle(levels);
         for (int i = 0; i < levels; i++) {
             for (int j = 0; j < (levels - i - 1); j++) {
                 System.out.printf("%2s", "");
             }
             for (int j = 0; j < (i + 1); j++) {
-                System.out.printf("%4d", calculateValue(i, j));
+                System.out.printf("%4d", matrix[i][j]);
             }
             System.out.println();
         }
+    }
+
+    public static int[][] createPascalTriangle(int levels) {
+        int[][] result = new int[levels][levels];
+        for (int i = 0; i < levels; i++) {
+            for (int j = 0; j < (i + 1); j++) {
+                result[i][j] = calculateValue(i, j);
+            }
+        }
+        return result;
     }
 
     private static int calculateValue(int row, int column) {
