0e074b28daee1634b27bce3e88e32546a93645bc
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-04-25 18:16:54 +0200

    Reformated MergeSort algorithm

--- src/main/java/pl/coderstrust/letssortandcomparethetimes/MergeSort.java
+++ src/main/java/pl/coderstrust/letssortandcomparethetimes/MergeSort.java
@@ -3,48 +3,58 @@ package pl.coderstrust.letssortandcomparethetimes;
 public class MergeSort implements SortingMethod {
 
     public int[] sort(int[] array) {
-        return sort(array, 0, array.length);
+        if (array.length <= 1) {
+            return array;
+        }
+        return sort(array, 0, array.length - 1);
     }
 
     public int[] sort(int[] array, int beginIndex, int endIndex) {
-        if (endIndex - beginIndex <= 1) {
+        if (endIndex - beginIndex + 1 == 1) {
             return array;
         }
         int leftBeginIndex = beginIndex;
-        int leftEndIndex = ((endIndex - beginIndex) / 2) - 1;
-        int rightBeginIndex = ((endIndex - beginIndex) / 2);
+        int leftEndIndex = beginIndex + (endIndex - beginIndex) / 2;
+        int rightBeginIndex = beginIndex + ((endIndex - beginIndex) / 2) + 1;
         int rightEndIndex = endIndex;
+        if (rightEndIndex >= array.length) {
+            rightEndIndex--;
+        }
         sort(array, leftBeginIndex, leftEndIndex);
         sort(array, rightBeginIndex, rightEndIndex);
-
         return merge(array, leftBeginIndex, leftEndIndex, rightBeginIndex, rightEndIndex);
     }
 
     public static int[] merge(int[] array, int leftBeginIndex, int leftEndIndex, int rightBeginIndex, int rightEndIndex) {
-        int i = leftBeginIndex, j = rightBeginIndex;
-        int resultIndex = leftBeginIndex;
-        int leftLength = leftEndIndex - leftBeginIndex;
-        int rightLength = rightEndIndex - rightBeginIndex;
-        while (i < leftLength && j < rightLength) {
+        int i = leftBeginIndex;
+        int j = rightBeginIndex;
+        int leftLength = leftEndIndex - leftBeginIndex + 1;
+        int rightLength = rightEndIndex - rightBeginIndex + 1;
+        int[] result = new int[leftLength + rightLength];
+        int resultIndex = 0;
+        while (i - leftBeginIndex < leftLength && j - rightBeginIndex < rightLength) {
             if (array[i] >= array[j]) {
-                array[resultIndex] = array[j];
+                result[resultIndex] = array[j];
                 j++;
                 resultIndex++;
             } else {
-                array[resultIndex] = array[i];
+                result[resultIndex] = array[i];
                 i++;
                 resultIndex++;
             }
         }
-        while (i < leftLength) {
-            array[resultIndex] = array[j];
+        while (i - leftBeginIndex < leftLength) {
+            result[resultIndex] = array[i];
+            i++;
+            resultIndex++;
+        }
+        while (j - rightBeginIndex < rightLength) {
+            result[resultIndex] = array[j];
             j++;
             resultIndex++;
         }
-        while (j < rightLength) {
-            array[resultIndex] = array[i];
-            i++;
-            resultIndex++;
+        for (int k = 0, m = leftBeginIndex; k < result.length; k++, m++) {
+            array[m] = result[k];
         }
         return array;
     }
