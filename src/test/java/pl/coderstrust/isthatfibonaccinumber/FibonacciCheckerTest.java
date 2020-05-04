54493c4be0b87bb3145cd164c8160ddf6272aa09
LukaszOsiniak <lukasz.osiniak@gmail.com>
2020-05-04 12:00:26 +0200

    Refactored tests

--- src/test/java/pl/coderstrust/isthatfibonaccinumber/FibonacciCheckerTest.java
+++ src/test/java/pl/coderstrust/isthatfibonaccinumber/FibonacciCheckerTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -22,26 +20,19 @@ public class FibonacciCheckerTest {
             //when
             boolean isFibonacci = FibonacciChecker.isFibonacciNumber(Long.parseLong(number));
             //then
-            assertTrue(isFibonacci);
+            assertTrue(isFibonacci, "Failed at number: " + number);
         }
     }
 
     @Test
-    public void shouldReturnFalseForNotFibonacciNumber() {
+    public void shouldReturnFalseForNotFibonacciNumber() throws IOException {
         //given
-        List<Long> nonFibonacci = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < 1000000; i++) {
-            long randomNumber = random.nextLong();
-            if (!FibonacciChecker.isFibonacciNumber(randomNumber)) {
-                nonFibonacci.add(random.nextLong());
-            }
-        }
-        for (Long number : nonFibonacci) {
+        List<String> nonFibonacciNumbers = Files.readAllLines(Path.of("src\\test\\resources\\nonFibonacciNumbers.txt"));
+        for (String number : nonFibonacciNumbers) {
             //when
-            boolean isFibonacci = FibonacciChecker.isFibonacciNumber(number);
+            boolean isFibonacci = FibonacciChecker.isFibonacciNumber(Long.parseLong(number));
             //then
-            assertFalse(isFibonacci);
+            assertFalse(isFibonacci, "Failed at number:" + number);
         }
     }
 }
